da1bd6e65816df06be93e42e27bbc489b85e5a90
Igor2020 <eger.eger.00@mail.ru>
2020-02-08 18:40:44 +0600

    Тертий коммит, установка vuex, json-server, axios, запуск джсон сервера который сомтрит на db.json так же первое обращение по API к серверу и получение данных и их отображение - изучение фронта вью джс, продолжение...

--- src/components/v-main-wrapper/v-catalog.vue
+++ src/components/v-main-wrapper/v-catalog.vue
@@ -1,100 +1,62 @@
 <template>
-    <div class="v-catalog">
-        <h1>Welcome to Catalog</h1>
+    <div class='v-catalog'>
+        <h1>Catalog</h1>
         <div class="v-catalog__list">
             <v-catalog-item
-                v-for="product in products"
-                :key="product.article"
-                v-bind:product_data="product"
-                @sendArticle = "showChildArticleInConsole"
+                    v-for="product in PRODUCTS"
+                    :key="product.article"
+                    :product_data="product"
+                    @addToCart="addToCart"
             />
         </div>
     </div>
 </template>
 
 <script>
-
-    import vCatalogItem from "./v-catalog-item.vue"
-
-    export default {
-        components: {
-            vCatalogItem
-        },
-        props: {
-        },
-
-            methods:{
-                showChildArticleInConsole(data){
-                    console.log(data)
-                }
-            },
-        data(){
-            return{
-
-  products: [
-    {
-      image: "1.jpg",
-      name: "T-shirt 1",
-      price: 2100,
-      article: "T1",
-      available: true,
-      category: "Мужские"
+  import vCatalogItem from './v-catalog-item'
+  import {mapActions, mapGetters} from 'vuex'
+  export default {
+    name: "v-catalog",
+    components: {
+      vCatalogItem
     },
-    {
-      image: "2.jpg",
-      name: "T-shirt 2",
-      price: 3150,
-      article: "T2",
-      available: true,
-      category: "Женские"
+    props: {},
+    data() {
+      return {
+      }
     },
-    {
-      image: "3.jpg",
-      name: "T-shirt 3",
-      price: 4200,
-      article: "T3",
-      available: false,
-      category: "Женские"
+    computed: {
+      ...mapGetters([
+        'PRODUCTS',
+      ]),
     },
-    {
-      image: "4.jpg",
-      name: "T-shirt 4",
-      price: 5300.1245512,
-      article: "T4",
-      available: true,
-      category: "Мужские"
+    methods: {
+      ...mapActions([
+        'GET_PRODUCTS_FROM_API',
+        'ADD_TO_CART'
+      ]),
+      addToCart(data) {
+        this.ADD_TO_CART(data)
+      }
     },
-    {
-      image: "5.jpg",
-      name: "T-shirt 5",
-      price: 6500,
-      article: "T5",
-      available: false,
-      category: "Женские"
-    },
-    {
-      image: "6.jpg",
-      name: "T-shirt 6",
-      price: 8700,
-      article: "T6",
-      available: true,
-      category: "Женские"
+    mounted() {
+      this.GET_PRODUCTS_FROM_API()
+      .then((response) => {
+        if (response.data) {
+          console.log('Data arrived!')
+        }
+      })
     }
-  ]
-
-            }
-        },
-    };
+  }
 </script>
 
-<style lang = "scss">
-    .v-catalog{
-        &__list{
-            display: flex;
-            flex-wrap: wrap;
-            justify-content:space-between;
-            align-items:center;
-
-        }
+<style lang="scss">
+  .v-catalog {
+    &__list {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
     }
+  }
 </style>
